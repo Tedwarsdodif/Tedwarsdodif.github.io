fc7042f1bd2a3c484dcf452b36b13ba5ecfc13c6
Hansel John <Hansel.John@warriorlife.net>
2018-10-04 08:34:00 -0700

    Learning JavaScript/Finished Lesson 2
    The only #2!!!

---
+++ scripts/communication.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+function button (){
+alert("scammed");
+}
+function butin(){
+alert("scamerd");
+}
+function buttoni (){
+	alert("scerasm");
+}
+function butoinnt (){
+	alert("scamerrdded");
+}
---
+++ scripts/me.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+	var name= "Hansel";
+		var age= "11";
+		var color= "green";
+		
+document.write(name + age + color);
